f7786d1f6d8983e60ec3e640b3f48f314b64569f
Johanne <johanne_lewis@hotmail.com>
2019-06-01 13:51:52 +0100

    Template for commnents

---
+++ admin/comments.php
@@ -0,0 +1,29 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+        
+
+
+           <!-- TOP MENU ITEM -->
+
+            <?php include "includes/top_navigation.php"; ?>
+
+           <!-- CLOSE TOP MENU ITEM-->
+
+            <!-- SIDEBAR MENU ITEMS -->
+             <?php include "includes/side_navigation.php"; ?>
+            <!-- CLOSE SIDEBAR MENU ITEMS -->
+           
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+          <?php include "includes/comments_content.php"; ?>
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
