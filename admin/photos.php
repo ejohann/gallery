63b8a30e786541bace86dfec4eb05c7133fc5083
Johanne <johanne_lewis@hotmail.com>
2019-06-01 13:59:04 +0100

    Template for photos page

---
+++ admin/photos.php
@@ -0,0 +1,49 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+        
+
+
+           <!-- TOP MENU ITEM -->
+
+            <?php include "includes/top_navigation.php"; ?>
+
+           <!-- CLOSE TOP MENU ITEM-->
+
+            <!-- SIDEBAR MENU ITEMS -->
+             <?php include "includes/side_navigation.php"; ?>
+            <!-- CLOSE SIDEBAR MENU ITEMS -->
+           
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+          <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header"> Photos <small>Subheading</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Blank Page
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
