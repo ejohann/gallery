a4a6edc04e8686895af3353cf6a4eab598a90315
Johanne <johanne_lewis@hotmail.com>
2019-06-02 19:18:07 +0100

    Setup DB connection

---
+++ admin/includes/config.php
@@ -0,0 +1,22 @@
+<?php
+
+
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'gallery_db';
+
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if($connection)
+ {
+ 	echo "We are connected";
+ }
+ else
+  {
+  	die("Connection failed " . mysqli_error($connection));
+  }
+
+
+?>
