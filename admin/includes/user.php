95f4c639ceac2dde32e31eb795a4e81fe8654957
Johanne <johanne_lewis@hotmail.com>
2019-06-03 12:37:49 +0100

    Create function to instantiate user object

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -33,6 +33,18 @@
       return $result_set;
     }
 
+    public static instantiation($user_details)
+     {
+        $new_object = new self; 
+
+        $new_object->id = $user_details['id'];
+        $new_object->username = $user_details['username'];
+        $new_object->password = $user_details['password'];
+        $new_object->user_firstname = $user_details['user_firstname'];
+        $new_object->user_lastname = $user_details['user_lastname'];
+
+     }
+
   }
 
 
