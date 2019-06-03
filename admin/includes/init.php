6a206e892ea39e8b4eb8bb332b025ff695478b1d
Johanne <johanne_lewis@hotmail.com>
2019-06-03 23:43:33 +0100

    Made files in file path require once instead of include

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,9 +1,9 @@
 <?php 
 
   require_once("functions.php");
-  include("database.php");
-  include("user.php");
-  include("session.php");
+  require_once("database.php");
+  require_once("user.php");
+  require_once("session.php");
 
 
 ?>
