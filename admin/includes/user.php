f23ba2f6d511d1d3b668d303a8789621dd6d3574
Johanne <johanne_lewis@hotmail.com>
2019-06-03 11:49:29 +0100

    Create a static function to return user details by user id

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -12,6 +12,14 @@
     }
   
 
+public static function find_user_by_id($user_id)
+    {
+      global $database;
+      $result_set = $database->query("SELECT * FROM users WHERE id={$user_id}");
+      return $result_set;
+    }
+  
+
   }
 
 
