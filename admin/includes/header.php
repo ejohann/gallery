910fa131bbb6c25e13783aa158b78e1d2e6fa1b5
Johanne <johanne_lewis@hotmail.com>
2019-06-04 00:40:48 +0100

    Redirect the user to the login page if not logged in

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,6 +1,15 @@
 <?php ob_start(); ?>
 <?php require_once("init.php"); ?>
 
+<?php 
+ 
+ if(!$session->is_signed_in())
+  {
+    header("Location: login.php");
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
