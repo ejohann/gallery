109c26749802b2046064d1e4e4f2ac61c4b8f5dc
Johanne <johanne_lewis@hotmail.com>
2019-06-03 23:41:40 +0100

    Delete code no longer needed

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,10 +1,9 @@
 <?php 
 
-//include("new_config.php");
-require_once("functions.php");
-include("database.php");
-include("user.php");
-include("session.php");
+  require_once("functions.php");
+  include("database.php");
+  include("user.php");
+  include("session.php");
 
 
 ?>
