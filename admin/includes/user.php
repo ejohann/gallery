f631382400b538f47b79379ba6e9075a82d6de91
Johanne <johanne_lewis@hotmail.com>
2019-06-03 11:05:53 +0100

    Create find all users function in user class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,10 +2,16 @@
 
  class User
   {
+   
 
+   public function find_all_users()
+    {
+      global $database;
+      $result_set = $database->query("SELECT * FROM users");
+      return $result_set;
+    }
+  
 
-
-  	
   }
 
 
