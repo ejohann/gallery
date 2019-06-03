8bb3dc6b658a9d92aff92086a951fcf72660b3fe
Johanne <johanne_lewis@hotmail.com>
2019-06-03 14:45:25 +0100

    Replaced deprecated autoload function

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,6 +1,6 @@
 <?php 
 
-  function __autoload($class)
+  function class_auto_loader($class)
    {
      $class = strtolower($class);
      $the_path = "includes/{$class}.php";
@@ -15,7 +15,7 @@
 
    }
 
-
+ spl_autoload_register('class_auto_loader');
 
 
 ?>
