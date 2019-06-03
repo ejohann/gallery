c82228ed6bc7e541534b20977d6655bbe754a3b6
Johanne <johanne_lewis@hotmail.com>
2019-06-04 00:31:09 +0100

    Create a method to logout a user

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -38,15 +38,23 @@ class Session
       }
 
 
-      public function login($user)
-       {
-         if($user)
-          { 
-             $this->user_id = $_SESSION['user_id'] = $user->id;
-             $this->signed_in = true;
-          }
+     public function login($user)
+      {
+        if($user)
+         { 
+            $this->user_id = $_SESSION['user_id'] = $user->id;
+            $this->signed_in = true;
+         }
+      }
+   
+    public function logout()
+      {
 
-       }
+      	unset($_SESSION['user_id']);
+      	unset($this->user_id);
+      	$this->signed_in = false;
+      }
+
 
  }
 
