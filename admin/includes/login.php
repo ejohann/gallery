cd45ea3329fa9e02e260001125e8383c323ffa46
Johanne <johanne_lewis@hotmail.com>
2019-06-04 01:04:57 +0100

    Code for verifyinh post request from form

--- admin/includes/login.php
+++ admin/includes/login.php
@@ -3,11 +3,32 @@
 
 <?php 
 
-if($session->is_signed_in())
- {
- 	redirect("../index.php");
- }
+  if($session->is_signed_in())
+   {
+ 	  redirect("../index.php");
+   }
 
+  if(isset($_POST['login']))
+   {
+     $username = trim($_POST['username']);
+     $password = trim($_POST['password']);
 
+     if($user_found)
+      {
+      	$session->login($user_found);
+      	redirect("../index.php");
+      }
+      else
+       {
+         $the_message = "Username or password is incorrect";
+
+       }
+  }
+  else
+  {
+    $username = "";
+    $password = "";
+
+  }
 
 ?>
