38e222507f9dff1b29a4947c92687ec533c559bc
Johanne <johanne_lewis@hotmail.com>
2019-06-06 13:00:03 +0100

    Add instantiation and has_the_attribute methods

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -21,7 +21,6 @@ class Db_object
       return !empty($result_array) ? array_shift($result_array) : false;
     }
 
-
   
     // runs any query and returns an object array
    public static function run_this_query($sql)
@@ -37,7 +36,27 @@ class Db_object
     }
 
 
-    
+     // makes an object from db result
+    public static function instantiation($user_details)
+     {
+       $new_object = new self; 
+       foreach($user_details as $the_attribute => $value)
+       	 {
+             if($new_object->has_the_attribute($the_attribute))
+              {
+              	$new_object->$the_attribute = $value; 
+              }
+         }
+        return $new_object;
+     }
+
+     // check if an attribute exists in an object
+     private function has_the_attribute($attribute)
+      {
+        $object_properties = get_object_vars($this);
+        return array_key_exists($attribute, $object_properties);
+      }
+
 
  }
 
