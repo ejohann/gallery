2d5ba0449ad34bdaa0592f353507dc420a17bd59
Johanne <johanne_lewis@hotmail.com>
2019-06-06 12:52:57 +0100

    Add find by id and find all methods

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -3,9 +3,25 @@
 class Db_object 
  {
 
+   public static function find_all()
+    {
+     //  global $database;
+      return self::run_this_query("SELECT * FROM " .self::$db_table. "");
+     // return $result_set;
+    }
+  
+
+   public static function find_by_id($id)
+    {
+      global $database;
+      $id = $database->escape($id);
+      $result_array = self::run_this_query("SELECT * FROM " .self::$db_table. " WHERE id={$id}");
+      return !empty($result_array) ? array_shift($result_array) : false;
+    }
 
 
- 	
+
+    
  }
 
 
