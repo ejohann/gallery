5dd433662f72ddf1343194c750f52f0ac8c2eef2
Johanne <johanne_lewis@hotmail.com>
2019-06-10 18:22:57 +0100

    If no get request redirect user, else get the photo by the id

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -2,6 +2,13 @@
 <?php if(!$session->is_signed_in()){ redirect("login.php"); } ?>
 
 <?php 
-  echo "It works";
+  if(empty($_GET['photo_id']))
+   {
+   	redirect("../index.php");
+   }
+
+   $photo = Photo::find_by_id($database->escape($_GET['photo_id']));
+
+   echo $photo->photo_title;
  
 ?>
