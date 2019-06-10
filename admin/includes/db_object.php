5f1c8d22d1aed2e8260684239e74e0a50fc19059
Johanne <johanne_lewis@hotmail.com>
2019-06-10 10:26:08 +0100

    Add methods from user class

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -59,6 +59,89 @@ class Db_object
       }
 
 
+      protected function properties() 
+     {
+      //return get_object_vars($this);
+      $properties = array();
+
+      foreach(self::$db_table_fields as $db_field)
+       {
+           if($this->has_the_attribute($db_field))
+             {
+                $properties[$db_field] = $this->$db_field;
+             }
+
+       }
+       return $properties;
+     }
+
+
+    protected function clean_properties()
+     {
+       global $database;
+       $clean_properties = array();
+
+       foreach($this->properties() as $key => $value)
+        {
+           $clean_properties[$key] = $database->escape($value);
+        }
+       return $clean_properties;
+     }
+
+    public function save()
+     {
+       return isset($this->id) ? $this->update() : $this->create();
+     }
+
+    public function create()
+     {
+         global $database;
+         $properties = $this->clean_properties();
+         $insert_user = "INSERT INTO " .self::$db_table. " (" .implode(",", array_keys($properties)). ") ";
+         $insert_user .= "VALUES ('". implode("','", array_values($properties)) ."')";
+          
+          if($database->query($insert_user))
+            {
+               $this->id = $database->the_insert_id();
+               return true;
+            }
+          else
+            {
+              return false;
+            }
+     }
+
+    public function update()
+     {
+         global $database;
+         $properties = $this->clean_properties();
+         $properties_pairs = array();
+
+         foreach($properties as $key => $value)
+           {
+
+             $properties_pairs[] = "{$key}='{$value}'";
+           }
+ 
+         $update_user = "UPDATE " .self::$db_table. " SET ";
+         $update_user .= implode(",",  $properties_pairs);
+         $update_user .= " WHERE id = " .$database->escape($this->id). "";
+          
+        $database->query($update_user);
+
+        return (mysqli_affected_rows($database->connection) == 1) ? true : false;   
+     }
+
+    public function delete()
+     {
+       global $database;
+       $user_id = $database->escape($this->id);
+       $delete_user = "DELETE FROM " .self::$db_table. " WHERE id = $user_id";
+       
+       return ($database->query($delete_user)) ? true : false ;
+     }
+
+
  }
 
 
