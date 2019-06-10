7503b0c51a1ad76f83301bea9a979079707d1328
Johanne <johanne_lewis@hotmail.com>
2019-06-10 20:32:23 +0100

    Link scripts.js file

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
-
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
