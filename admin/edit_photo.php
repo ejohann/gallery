19c05223296a9acf12ee288da5cac9c1cf82e9eb
Johanne <johanne_lewis@hotmail.com>
2019-06-10 19:07:55 +0100

    Create markup for edit photo page

---
+++ admin/edit_photo.php
@@ -0,0 +1,47 @@
+<?php include("includes/header.php"); ?>
+<?php if(!$session->is_signed_in()){ redirect("login.php"); } ?>
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+        
+
+
+           <!-- TOP MENU ITEM -->
+
+            <?php include "includes/top_navigation.php"; ?>
+
+           <!-- CLOSE TOP MENU ITEM-->
+
+            <!-- SIDEBAR MENU ITEMS -->
+             <?php include "includes/side_navigation.php"; ?>
+            <!-- CLOSE SIDEBAR MENU ITEMS -->
+           
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+          <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header"> Photos <small>Subheading</small>
+                        </h1>
+
+                        <div class="col-md-12">
+                            
+                           
+                        </div>      
+                       
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
