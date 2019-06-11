c45870346ed6a1bde4f5a33bd6ae543bfb3937c6
Johanne <johanne_lewis@hotmail.com>
2019-06-11 10:23:41 +0100

    Remove custom errors and upload errors array properties as it is inherited from parent class

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -13,21 +13,8 @@
       public $photo_alternate_text;
       public $photo_type;
       public $photo_size;
-      
       public $tmp_path;
       public $upload_directory = "images";
-      public $custom_errors_array = array();
-      public $upload_errors_array = array(
-       UPLOAD_ERR_OK => "File uploaded successfully",
-       UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize directive",
-       UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive",
-       UPLOAD_ERR_PARTIAL => "The uploaded file was partially uploaded",
-       UPLOAD_ERR_NO_FILE => "No file was uploaded",
-       UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary directory",
-       UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk",
-       UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload"
-        ); 
-
      
        //method for passing the uploaded file as an argument
       public function set_file($file)
