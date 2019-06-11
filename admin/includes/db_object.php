5758387aa42bf8a7795ddb60c9db4a0247fa1a0c
Johanne <johanne_lewis@hotmail.com>
2019-06-11 10:22:13 +0100

    Add custom errors and upload errors properties to object

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -3,6 +3,19 @@
 class Db_object 
  {
 
+    public $custom_errors_array = array();
+    public $upload_errors_array = array(
+       UPLOAD_ERR_OK => "File uploaded successfully",
+       UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize directive",
+       UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive",
+       UPLOAD_ERR_PARTIAL => "The uploaded file was partially uploaded",
+       UPLOAD_ERR_NO_FILE => "No file was uploaded",
+       UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary directory",
+       UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk",
+       UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload"
+        ); 
+    
+
    // returns all records in a table
    public static function find_all()
     {
