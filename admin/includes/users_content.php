b82f8d8dd15d34ef5df2ff426aa64ff66810444c
Johanne <johanne_lewis@hotmail.com>
2019-06-11 09:15:56 +0100

    Create an add user button

--- admin/includes/users_content.php
+++ admin/includes/users_content.php
@@ -3,8 +3,8 @@
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header"> Users <small>Subheading</small>
-                        </h1>
+                        <h1 class="page-header"> Users </h1>
+                        <a href="add_user.php" class="btn btn-primary">Add User</a>
                         <div class="col-md-12">
                             
                             <table class="table table-hover">
