7f1b8e47bc18ea10149c52fd69740b6ac768ddf4
Johanne <johanne_lewis@hotmail.com>
2019-06-11 08:24:20 +0100

    Display users information

--- admin/includes/users_content.php
+++ admin/includes/users_content.php
@@ -5,7 +5,34 @@
                     <div class="col-lg-12">
                         <h1 class="page-header"> Users <small>Subheading</small>
                         </h1>
-                        
+                        <div class="col-md-12">
+                            
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Users</th>
+                                        <th>ID</th>
+                                        <th>Username</th>
+                                        <th>First name</th>
+                                        <th>Last name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                  
+                                    <?php $users = User::find_all(); ?>
+                                    <?php foreach($users as $user): ?>
+                                        <tr>
+                                         <td>  </td>
+                                         <td><?php echo $user->id; ?></td>
+                                         <td><?php echo $user->username; ?></td>
+                                         <td><?php echo $user->user_firstname; ?></td>
+                                         <td><?php echo $user->user_lastname; ?></td>
+                                         </tr>
+                                      <?php endforeach;  ?>
+                                 
+                                </tbody>
+                            </table>
+                        </div>     
                     </div>
                 </div>
                 <!-- /.row -->
