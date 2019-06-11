a707934b7c0332ca4aa86fe004dcf0cd6b6d4269
Johanne <johanne_lewis@hotmail.com>
2019-06-11 16:51:18 +0100

    Display all comments

--- admin/includes/comments_content.php
+++ admin/includes/comments_content.php
@@ -5,7 +5,36 @@
                     <div class="col-lg-12">
                         <h1 class="page-header"> Comments <small>Subheading</small>
                         </h1>
-                         
+                                 <div class="col-md-12">
+                            
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        
+                                        <th>ID</th>
+                                        <th>Photo ID</th>
+                                        <th>Author</th>
+                                        <th>Content</th>
+                                     
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                  
+                                    <?php $comments = Comment::find_all(); ?>
+                                    <?php foreach($comments as $comment): ?>
+                                        <tr>
+                                         
+                                         <td><?php echo $comment->id; ?></td>
+                                         <td><?php echo $comment->photo_id; ?> </td>
+                                         <td><?php echo $comment->author; ?> </td>
+                                         <td><?php echo $comment->content; ?></td>
+                                         
+                                         </tr>
+                                      <?php endforeach;  ?>
+                                 
+                                </tbody>
+                            </table>
+                        </div>  
                     </div>
                 </div>
                 <!-- /.row -->
