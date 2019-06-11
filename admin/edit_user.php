48c2d7fc700293bd67ce3d91527728e3bf5facfd
Johanne <johanne_lewis@hotmail.com>
2019-06-11 10:30:31 +0100

    Redirect user if no user_id in get request else create a user object with user id

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -1,2 +1,16 @@
 <?php include("includes/header.php"); ?>
 <?php if(!$session->is_signed_in()){ redirect("login.php"); } ?>
+
+<?php
+
+ if(empty($_GET['user_id']))
+   {
+    redirect("../index.php");
+   }
+  else
+   {
+     $user_id = $database->escape($_GET['user_id']);
+     $user = User::find_by_id($user_id);
+   }
+
+  ?>
