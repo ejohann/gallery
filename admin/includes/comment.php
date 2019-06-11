014313cfcde365db5e322607533648a2bc1f8ad8
Johanne <johanne_lewis@hotmail.com>
2019-06-11 14:31:27 +0100

    Create properties for comment object

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -3,8 +3,13 @@
 
  class Comment extends Db_object
     {
-
-   
+      protected static $db_table = "comments";
+      protected static $db_table_fields = array('id','photo_id','author','body');
+      public $id;
+      public $photo_id;
+      public $author;
+      public $body;
+       
 
 
     }
