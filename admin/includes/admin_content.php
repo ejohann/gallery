88b2a47c132382b062f25fabd2fc9b464c624e60
Johanne <johanne_lewis@hotmail.com>
2019-06-11 21:37:56 +0100

    Display comment count

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -87,7 +87,7 @@
                                         <i class="fa fa-support fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class="huge">8</div>
+                                        <div class="huge"><?php echo Comment::count_all(); ?></div>
                                         <div>Comments</div>
                                     </div>
                                 </div>
