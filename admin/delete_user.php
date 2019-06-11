35941fbf75ecca84cad92fd81410aa9b5a48b543
Johanne <johanne_lewis@hotmail.com>
2019-06-11 09:04:08 +0100

    Rdirect user if no user id in get request

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -1,2 +1,11 @@
 <?php include("includes/init.php"); ?>
 <?php if(!$session->is_signed_in()){ redirect("login.php"); } ?>
+
+<?php 
+  if(empty($_GET['user_id']))
+   {
+   	redirect("../index.php");
+   }
+
+   
+?>
