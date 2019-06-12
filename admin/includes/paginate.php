86929771de952f64a9a188fd4d33388d6fa5c471
Johanne <johanne_lewis@hotmail.com>
2019-06-12 08:55:32 +0100

    Assign values to object

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -7,8 +7,10 @@
 
       public function __construct($page=1, $items_per_page=4, $items_total_count=0)
        {
+         $this->page = $page;
+         $this->items_per_page = $items_per_page;
+         $this->items_total_count = $items_total_count;
 
-       	
        }
       
 
