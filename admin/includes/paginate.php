5c3a7df22d780c2355ab18841cfd0204b3e22abc
Johanne <johanne_lewis@hotmail.com>
2019-06-12 08:53:13 +0100

    Create ac onstructor method with default values

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -5,6 +5,12 @@
       public $items_per_page;
       public $items_total_count;
 
+      public function __construct($page=1, $items_per_page=4, $items_total_count=0)
+       {
+
+       	
+       }
+      
 
    }
 
