a4de0d8eef3c1593935484ef5b178ed9234bfe6c
Johanne <johanne_lewis@hotmail.com>
2019-06-12 20:26:33 +0100

    Create variables, target the delete user button and grab the href url

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 
+  var user_href;
+  var user_href_split;
+  var user_id;
+
   $(".modal_thumbnails").click(function(){
 
     $("#set_user_image").prop('disabled', false);
-   
-
+     
+    user_href = $("#user-id-del").prop('href');
+    
   });
 
 
