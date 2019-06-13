178ccec97f58004e20939e5bcdf6603299be811c
Johanne <johanne_lewis@hotmail.com>
2019-06-13 12:54:59 +0100

    Call method for returning image details for ajax request

--- admin/includes/ajax_code.php
+++ admin/includes/ajax_code.php
@@ -3,9 +3,15 @@
 <?php 
    
   $user = new User();
+  $photo = new Photo();
   if(isset($_POST['image_name']))
    {
    	 $user->ajax_save_user_image($_POST['image_name'], $_POST['user_id']);
+   }
+
+  if(isset($_POST['photo_id']))
+   {
+   	$photo->ajax_show_photo_info($_POST['photo_id']);
    }
 
 ?>
