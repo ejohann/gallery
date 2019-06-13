e6f3f66955e68b9f917bf6366c506d3af95a18d5
Johanne <johanne_lewis@hotmail.com>
2019-06-13 11:28:35 +0100

    Instantiate a user object and use method to save the user image

--- admin/includes/ajax_code.php
+++ admin/includes/ajax_code.php
@@ -1,11 +1,11 @@
 <?php require_once("init.php"); ?>
 
 <?php 
-
+   
+  $user = new User();
   if(isset($_POST['image_name']))
    {
-
-   	 echo "This is data from ajax...";
+   	 $user->ajax_save_user_image($_POST['image_name']), $_POST['user_id']));
    }
 
 ?>
