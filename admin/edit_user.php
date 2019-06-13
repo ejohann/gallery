4ff30892b1191aaa25ec1a8f2685cde91d3cbe7e
Johanne <johanne_lewis@hotmail.com>
2019-06-13 11:55:31 +0100

    Add class to user image div

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -65,7 +65,7 @@
         <div class="col-lg-12">
           <h1 class="page-header"> Edit User <small>Subheading</small></h1>
              
-             <div class="col-md-6">
+             <div class="col-md-6 user_image_box">
                 <a href="#" data-toggle="modal" data-target="#photo-library" ><img class="img-responsive" src="<?php echo $user->image_placeholder() ;?>" /></a>
              </div>
              
