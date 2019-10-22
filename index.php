a49e8ad96fc1324cb90a07233eeeb92e0b21729a
Johanne <johanne_lewis@hotmail.com>
2019-10-22 16:20:04 +0100

    Tidy up code

--- index.php
+++ index.php
@@ -1,91 +1,61 @@
-<?php include("includes/header.php"); ?>
+    <?php include("includes/header.php"); ?>
 
-<?php 
-  
-  $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
-  $items_per_page = 4;
-  $items_total_count = Photo::count_all();
+    <?php 
+      $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+      $items_per_page = 4;
+      $items_total_count = Photo::count_all();
 
-  $paginate = new Paginate($page, $items_per_page, $items_total_count);
+      $paginate = new Paginate($page, $items_per_page, $items_total_count);
 
-  $select_by_page = "SELECT * FROM photos ";
-  $select_by_page .= "LIMIT {$paginate->items_per_page} ";
-  $select_by_page .= "OFFSET {$paginate->offset()}";
+      $select_by_page = "SELECT * FROM photos ";
+      $select_by_page .= "LIMIT {$paginate->items_per_page} ";
+      $select_by_page .= "OFFSET {$paginate->offset()}";
 
-  $photos = Photo::run_this_query($select_by_page);
-   
-  //$photos = Photo::find_all();
-  
-?>
+      $photos = Photo::run_this_query($select_by_page);
+    ?>
+
+    <div class="row">
+        <div class="col-md-12">
+          <div class="thumbnails row">
+            <?php foreach($photos as $photo): ?>
+              <div class="col-xs-6 col-md-3">
+                <a class="thumbnail" href="photo.php?photo_id=<?php echo $photo->id; ?>">
+                  <img class="img-responsive home_page_photo" src="admin/<?php echo $photo->image_path(); ?>"  alt="" /> 
+                </a>    
+              </div>
+          <?php endforeach; ?>
+        </div>
 
         <div class="row">
+          <ul class="pager">
+            <?php 
+              if($paginate->page_total() > 1)
+               {
+                  if($paginate->has_next())
+                   {
+                      echo "<li class='next'><a href='index.php?page={$paginate->next_page()}'>Next</a></li> ";
+                   }
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-12">
+                  for($i=1; $i<= $paginate->page_total(); $i++)
+                   {
+                      if($i == $paginate->current_page)
+                       {
+                          echo "<li class='active'>{$i}</li>";
+                       }
+                      else
+                       {
+                          echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                       }
+                   }
 
-           <div class="thumbnails row">
-            <?php foreach($photos as $photo): ?>
-             
+                  if($paginate->has_previous())
+                   {
+                      echo "<li class='previous'><a href='index.php?page={$paginate->previous_page()}'>Previous</a></li> ";
+                   }
+               }
+            ?>
+          </ul>        
+        </div>
+    </div>
 
-                <div class="col-xs-6 col-md-3">
-
-                 <a class="thumbnail" href="photo.php?photo_id=<?php echo $photo->id; ?>"><img class="img-responsive home_page_photo" src="admin/<?php echo $photo->image_path(); ?>"  alt="" /> </a>    
-             
-                </div>
-
-             <?php endforeach; ?>
-            </div>
-
-           <div class="row">
-               <ul class="pager">
-                  <?php 
-                     if($paginate->page_total() > 1)
-                      {
-                        if($paginate->has_next())
-                         {
-                           echo "<li class='next'><a href='index.php?page={$paginate->next_page()}'>Next</a></li> ";
-                         }
-
-                         for($i=1; $i<= $paginate->page_total(); $i++)
-                          {
-                            if($i == $paginate->current_page)
-                             {
-                                echo "<li class='active'>{$i}</li>";
-                             }
-                            else
-                            {
-                                echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-                            }
-                          }
-
-                        if($paginate->has_previous())
-                         {
-                           echo "<li class='previous'><a href='index.php?page={$paginate->previous_page()}'>Previous</a></li> ";
-                         }
-                      }
-
-                  ?>
-
-                   
-                  <!-- -->
-               </ul>        
-
-           </div>
-
-            </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <!--<div class="col-md-4">
-
-            
-                 <?php // include("includes/sidebar.php"); ?>
-
-
-
-         </div> -->
-        <!-- /.row -->
-
-        <?php include("includes/footer.php"); ?>
+    <?php include("includes/footer.php"); ?>
