fdf8fef65d15a63329031d5490a3ff38d9479e0a
Johanne <johanne_lewis@hotmail.com>
2019-10-23 10:18:26 +0100

    Fix bug in code

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,17 +1,17 @@
 <?php 
 
-  defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+  defined('DS') ? null : define('DS', '/');
   defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:' .DS. 'xampp' .DS. 'htdocs' .DS. 'gallery');
   defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', 'C:' .DS. 'xampp' .DS. 'htdocs' .DS. 'gallery' .DS. 'admin' .DS. 'includes');
 
-  require_once("functions.php");
-  require_once("database.php");
-  require_once("db_object.php");
-  require_once("user.php");
-  require_once("photo.php");
-  require_once("comment.php");
-  require_once("session.php");
-  require_once("paginate.php");
+  require_once("" .INCLUDES_PATH. "" .DS. "functions.php");
+  require_once("" .INCLUDES_PATH. "" .DS. "database.php");
+  require_once("" .INCLUDES_PATH. "" .DS. "db_object.php");
+  require_once("" .INCLUDES_PATH. "" .DS. "user.php");
+  require_once("" .INCLUDES_PATH. "" .DS. "photo.php");
+  require_once("" .INCLUDES_PATH. "" .DS. "comment.php");
+  require_once("" .INCLUDES_PATH. "" .DS. "session.php");
+  require_once("" .INCLUDES_PATH. "" .DS. "paginate.php");
 
 
 ?>
